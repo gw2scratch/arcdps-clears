cd980df635f0e3d038fb713d8568eb0dde921c48
Jiří Sejkora <sejsel@sejsel.cz>
2021-11-29 05:49:07 +0100

    Move default settings to its own module

--- src/settings/mod.rs
+++ src/settings/mod.rs
@@ -1,42 +1,44 @@
+mod defaults;
+
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use std::error::Error;
 use std::fs::File;
 use std::hash::{Hash, Hasher};
-use std::io::{BufWriter, Write, Read};
+use std::io::{BufWriter, Read, Write};
 use std::sync::Mutex;
 use log::error;
 use uuid::Uuid;
 
 #[derive(Serialize, Deserialize)]
 pub struct Settings {
-    #[serde(default = "default_friends_api_url")]
+    #[serde(default = "defaults::friends_api_url")]
     pub friends_api_url: String,
-    #[serde(default = "default_api_keys")]
+    #[serde(default = "defaults::api_keys")]
     pub api_keys: Vec<ApiKey>,
-    #[serde(default = "default_friends")]
+    #[serde(default = "defaults::friends")]
     pub friend_list: Vec<Friend>,
-    #[serde(default = "default_friend_default_show_state")]
+    #[serde(default = "defaults::friend_default_show_state")]
     pub friend_default_show_state: bool,
-    #[serde(default = "default_check_updates")]
+    #[serde(default = "defaults::check_updates")]
     pub check_updates: bool,
-    #[serde(default = "default_short_names")]
+    #[serde(default = "defaults::short_names")]
     pub short_names: bool,
-    #[serde(default = "default_my_clears_style")]
+    #[serde(default = "defaults::my_clears_style")]
     pub my_clears_style: ClearsStyle,
-    #[serde(default = "default_friends_clears_style")]
+    #[serde(default = "defaults::friends_clears_style")]
     pub friends_clears_style: ClearsStyle,
-    #[serde(default = "default_main_window_keybind")]
+    #[serde(default = "defaults::main_window_keybind")]
     pub main_window_keybind: Option<usize>,
-    #[serde(default = "default_api_window_keybind")]
+    #[serde(default = "defaults::api_window_keybind")]
     pub api_window_keybind: Option<usize>,
-    #[serde(default = "default_close_window_with_escape")]
+    #[serde(default = "defaults::close_window_with_escape")]
     pub close_window_with_escape: bool,
-    #[serde(default = "default_hide_in_loading_screens")]
+    #[serde(default = "defaults::hide_in_loading_screens")]
     pub hide_in_loading_screens: bool,
-    #[serde(default = "default_main_window_show_bg")]
+    #[serde(default = "defaults::main_window_show_bg")]
     pub main_window_show_bg: bool,
-    #[serde(default = "default_main_window_show_title")]
+    #[serde(default = "defaults::main_window_show_title")]
     pub main_window_show_title: bool,
     // Are you adding a new style option? Make sure to add to `reset_style()`!
 }
@@ -49,69 +51,6 @@ pub struct ClearsStyle {
     pub show_clears_table_row_names: bool,
     pub finished_clear_color: [f32; 4],
     pub unfinished_clear_color: [f32; 4],
-}
-
-fn default_my_clears_style() -> ClearsStyle {
-    ClearsStyle {
-        table_style: ClearsTableStyle::WingRows,
-        account_header_style: AccountHeaderStyle::CenteredText,
-        show_clears_table_headers: true,
-        show_clears_table_row_names: true,
-        finished_clear_color: [38. / 255., 199. / 255., 29. / 255., 177. / 255.],
-        unfinished_clear_color: [192. / 255., 24. / 255., 30. / 255., 136. / 255.],
-    }
-}
-
-fn default_friends_clears_style() -> ClearsStyle {
-    ClearsStyle {
-        table_style: ClearsTableStyle::SingleRow,
-        account_header_style: AccountHeaderStyle::CenteredText,
-        show_clears_table_headers: true,
-        show_clears_table_row_names: true,
-        finished_clear_color: [38. / 255., 199. / 255., 29. / 255., 177. / 255.],
-        unfinished_clear_color: [192. / 255., 24. / 255., 30. / 255., 136. / 255.],
-    }
-}
-
-fn default_friends_api_url() -> String {
-    "https://clears.gw2scratch.com/".to_string()
-}
-fn default_check_updates() -> bool {
-    true
-}
-fn default_api_keys() -> Vec<ApiKey> {
-    Vec::new()
-}
-fn default_friends() -> Vec<Friend> {
-    Vec::new()
-}
-fn default_friend_default_show_state() -> bool {
-    true
-}
-fn default_short_names() -> bool {
-    true
-}
-fn default_api_window_keybind() -> Option<usize> {
-    None
-}
-fn default_main_window_keybind() -> Option<usize> {
-    // C (for clears)
-    // Note that arcdps uses this as a default keybind, but it seems to be an uncommonly used one.
-    // As we eat the input, there should be no issue and the behavior should be obvious. The user
-    // can change the conflicting keybind in our plugin or in arcdps itself if needed.
-    Some(67)
-}
-fn default_close_window_with_escape() -> bool {
-    true
-}
-fn default_hide_in_loading_screens() -> bool {
-    false
-}
-fn default_main_window_show_bg() -> bool {
-    true
-}
-fn default_main_window_show_title() -> bool {
-    true
 }
 
 #[derive(Serialize, Deserialize, Eq, PartialEq, Clone, Copy)]
@@ -134,21 +73,15 @@ pub struct ApiKey {
     id: Uuid,
     key: String,
     data: ApiKeyData,
-    #[serde(default = "default_show_key_in_clears")]
+    #[serde(default = "defaults::show_key_in_clears")]
     show_key_in_clears: bool,
-    #[serde(default = "default_expanded_in_clears")]
+    #[serde(default = "defaults::expanded_in_clears")]
     expanded_in_clears: bool
 }
 
 fn new_api_key_id() -> Uuid {
     Uuid::new_v4()
 }
-
-fn default_show_key_in_clears() -> bool {
-    true
-}
-
-fn default_expanded_in_clears() -> bool { true }
 
 impl Hash for ApiKey {
     fn hash<H: Hasher>(&self, state: &mut H) {
@@ -163,8 +96,8 @@ impl ApiKey {
             id: new_api_key_id(),
             key: str.to_string(),
             data: ApiKeyData::empty(),
-            show_key_in_clears: default_show_key_in_clears(),
-            expanded_in_clears: default_expanded_in_clears()
+            show_key_in_clears: defaults::show_key_in_clears(),
+            expanded_in_clears: defaults::expanded_in_clears()
         }
     }
     pub fn change_key(&mut self, str: &str) {
@@ -326,30 +259,30 @@ impl Friend {
 impl Settings {
     fn default() -> Self {
         Settings {
-            friends_api_url: default_friends_api_url(),
-            api_keys: default_api_keys(),
-            friend_list: default_friends(),
-            friend_default_show_state: default_friend_default_show_state(),
-            check_updates: default_check_updates(),
-            short_names: default_short_names(),
-            my_clears_style: default_my_clears_style(),
-            friends_clears_style: default_friends_clears_style(),
-            main_window_keybind: default_main_window_keybind(),
-            api_window_keybind: default_api_window_keybind(),
-            close_window_with_escape: default_close_window_with_escape(),
-            hide_in_loading_screens: default_hide_in_loading_screens(),
-            main_window_show_bg: default_main_window_show_bg(),
-            main_window_show_title: default_main_window_show_title(),
+            friends_api_url: defaults::friends_api_url(),
+            api_keys: defaults::api_keys(),
+            friend_list: defaults::friends(),
+            friend_default_show_state: defaults::friend_default_show_state(),
+            check_updates: defaults::check_updates(),
+            short_names: defaults::short_names(),
+            my_clears_style: defaults::my_clears_style(),
+            friends_clears_style: defaults::friends_clears_style(),
+            main_window_keybind: defaults::main_window_keybind(),
+            api_window_keybind: defaults::api_window_keybind(),
+            close_window_with_escape: defaults::close_window_with_escape(),
+            hide_in_loading_screens: defaults::hide_in_loading_screens(),
+            main_window_show_bg: defaults::main_window_show_bg(),
+            main_window_show_title: defaults::main_window_show_title(),
             // Are you adding a new style option? Make sure to add to `reset_style()`!
         }
     }
 
     pub fn reset_style(&mut self) {
-        self.short_names = default_short_names();
-        self.my_clears_style = default_my_clears_style();
-        self.friends_clears_style = default_friends_clears_style();
-        self.main_window_show_bg = default_main_window_show_bg();
-        self.main_window_show_title = default_main_window_show_title();
+        self.short_names = defaults::short_names();
+        self.my_clears_style = defaults::my_clears_style();
+        self.friends_clears_style = defaults::friends_clears_style();
+        self.main_window_show_bg = defaults::main_window_show_bg();
+        self.main_window_show_title = defaults::main_window_show_title();
     }
 
     pub fn api_keys(&self) -> &Vec<ApiKey> {
